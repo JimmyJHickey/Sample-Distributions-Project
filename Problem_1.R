c8b0ed43b4be6563aa20dc226f9ba316093d5c0b
jimmyjhickey <jimmyjhickey@gmail.com>
2019-07-21 15:48:49 -0400

    Added plotting for problem 1

--- Problem_1.R
+++ Problem_1.R
@@ -4,6 +4,8 @@
 # Sampling Distribution of the Laplace Distribution.
 ###
 
+library(ggplot2)
+
 set.seed(1978)
 
 # Problem 1.
@@ -42,10 +44,9 @@ L_calc = function(ys){
 
 
 # K
-# Sum an array of inputs and divide by the length
+# K = sqrt(L)
 K_calc = function(ys){
-  n = length(ys)
-  return(1/n * sum(ys))
+  return(sqrt(L_calc(ys)))
 } 
 
 
@@ -62,11 +63,51 @@ for (j in 1:max_sample_size){
 
 # Plotting K and L
 
-plot(n, K_Matrix[1,], main="n vs K", 
-     xlab="n ", ylab="K_n", add = TRUE)
-
-for (i in 2:50){
-  points(n, K_Matrix[i,], col = i)
+plotting_function = function(
+                      sample_sizes,
+                      data_matrix,
+                      expected_mean,
+                      epsilon,
+                      title,
+                      y_lab){
   
+  # Create plot
+  plot(sample_sizes, data_matrix[1,], main=title, 
+       xlab="n ", ylab=y_lab, add = TRUE, col = alpha("black", 0.2), pch = 16)
+  
+  # Add additional data points
+  for (i in 2:nrow(K_Matrix)){
+    points(n, data_matrix[i,], col = alpha("black", 0.2), pch = 16)
+  }  
+  
+  # Add line at expected mean
+  abline(h = expected_mean, col="blue", lwd = 4)
+  
+  # Add epsilon lines
+  abline(h = expected_mean + epsilon, col = "red", lwd = 5, lty = 2)
+  abline(h = expected_mean - epsilon, col = "red", lwd = 5, lty = 2)
 }
 
+plotting_function(sample_sizes = n,
+                  data_matrix = K_Matrix,
+                  expected_mean = sqrt(50),
+                  epsilon = 3,
+                  title = "Estimates of K as n increases",
+                  y_lab = "K_n")
+
+plotting_function(sample_sizes = n,
+                  data_matrix = L_Matrix,
+                  expected_mean = 50,
+                  epsilon = 20,
+                  title = "Estimates of L as n increases",
+                  y_lab = "L_n")
+
+# These graphs both demonstrate that our RVs L and K are converging in probability
+# At each sample size n, the same number of samples (50) were taken. 
+# Notice the trend as the number of observations (n) in each samle increases.
+# It is clear that there is far less spread.
+# As n increases, the RVs are converging to the blue line.
+# The observed values of L are geting closer and closer to 50.
+# And the observed values of K are approaching sqrt(50). 
+# As n increases we could continue to shrink our epsilon bubble around the expected value
+# and we will continue to see this convergence.
